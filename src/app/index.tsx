363de9065dbceffd006654ddfaf171e398d23ae8
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-02-08 20:16:47 -0300

    Initial config

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View style={{ backgroundColor: "#fff", minHeight: "100%" }}>
+      <Text>teste</Text>
+      <Text>teste</Text>
+    </View>
+  );
+};
+export default App;
